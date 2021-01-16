a2f5c11227b8c256fb3052067cabb67602bbf8b2
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 16:47:16 +0330

    DetailFragment added to project

---
+++ app/src/main/java/com/example/navigationcomponentsample/ui/home/DetailFragment.kt
@@ -0,0 +1,6 @@
+package com.example.navigationcomponentsample.ui.home
+
+import androidx.fragment.app.Fragment
+import com.example.navigationcomponentsample.R
+
+class DetailFragment : Fragment(R.layout.fragment_detail)
