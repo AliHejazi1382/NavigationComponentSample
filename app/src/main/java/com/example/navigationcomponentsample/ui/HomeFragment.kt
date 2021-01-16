4d48e645a475b862c22cb827a5107b7dffa0c272
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 16:38:58 +0330

    HomeFragment added

---
+++ app/src/main/java/com/example/navigationcomponentsample/ui/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navigationcomponentsample.ui
+
+import androidx.fragment.app.Fragment
+import com.example.navigationcomponentsample.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
