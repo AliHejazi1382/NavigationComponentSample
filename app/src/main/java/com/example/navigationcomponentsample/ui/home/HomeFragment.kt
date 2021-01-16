4f5184e1f4fdccfc23d3d6f287533b2f2ebfe4fc
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 18:32:33 +0330

    Action of Home set

--- app/src/main/java/com/example/navigationcomponentsample/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/navigationcomponentsample/ui/home/HomeFragment.kt
@@ -1,7 +1,27 @@
 package com.example.navigationcomponentsample.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponentsample.R
+import com.example.navigationcomponentsample.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
+        binding.btnGoToDetail.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
