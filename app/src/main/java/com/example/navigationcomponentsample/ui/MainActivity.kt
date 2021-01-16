cf4e22cd33aa1f67d21488a82b49bc48464f8b64
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 16:11:15 +0330

    MainActivity added to ui package

--- app/src/main/java/com/example/navigationcomponentsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponentsample/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.navigationcomponentsample
+package com.example.navigationcomponentsample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navigationcomponentsample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
