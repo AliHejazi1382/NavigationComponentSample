6e915c8cb14fb3791e28cd1d2e968d306cc8b18a
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 17:25:19 +0330

    FlowerFragment added

---
+++ app/src/main/java/com/example/navigationcomponentsample/ui/list/FlowerFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navigationcomponentsample.ui.list
+
+import androidx.fragment.app.Fragment
+import com.example.navigationcomponentsample.R
+
+class FlowerFragment : Fragment(R.layout.fragment_flower) {
+}
