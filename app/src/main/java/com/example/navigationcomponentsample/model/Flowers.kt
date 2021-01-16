29867f4219a3c6f3d5105de6b14242f91278817d
AliHejazi1382 <mrhjz08@gmail.com>
2021-01-16 16:13:51 +0330

    The class Flowers added to handling data model

---
+++ app/src/main/java/com/example/navigationcomponentsample/model/Flowers.kt
@@ -0,0 +1,8 @@
+package com.example.navigationcomponentsample.model
+
+data class Flowers(val productId: Int,
+                    val category: String?,
+                    val instructions: String?,
+                    val name: String?,
+                    val photo: String?,
+                    val price: Double)
